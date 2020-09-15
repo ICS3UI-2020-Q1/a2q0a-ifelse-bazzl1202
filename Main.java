4c99c12ecf8a62efed1519fc52d25681c2acb549
bazzl1202 <bazzl1202@wrdsb.ca>
2020-09-15 14:01:03 +0000

    Says if you can vote and drive or not.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Let's the user know if they can drive or not.
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,32 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Define the minimum driving age
+    final int LICENSE_AGE = 16;
+
+    //define the minimum voting age
+    final int VOTING_AGE = 18;
+
+    // prompt the user for their LICENSE_AGE
+    System.out.println("Please enter your age:");
+    // get the age from the user
+    int age = input.nextInt();
+
+    // check to see if they can vote or drive
+    if( age >= VOTING_AGE){
+      System.out.println("You can vote");
+      System.out.println("You can legally drive");
+    } else if(age >= LICENSE_AGE){
+      // tell them they can drive
+      System.out.println("You can legally get your license");
+    } else {
+      System.out.println("Sorry, you can not drive yet!");
+    }
     
+
+
   }
 }
